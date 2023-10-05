699deea31ac58447b1d4aab4084ec2f3621197fc
Karthikeyan <karthikeyan162001@gmail.com>
2023-10-05 11:54:10 +0530

    robofriend

---
+++ src/Components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({name,email,id})=>{
+	
+	return(
+		<div className = "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+			<img alt="robo1" src={`https://robohash.org/${id}`} width="250" height="250"/>
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>
+
+
+		);
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+	{
+		id:1,
+		name : 'Alexa',
+		username : 'Liza',
+		email : 'alexaliza@april.biz' 
+	},
+	{
+		id:2,
+		name : 'Ferode',
+		username : 'Liza',
+		email : 'ferodeliza@april.biz' 
+	},
+	{
+		id:3,
+		name : 'Tafiya',
+		username : 'Liza',
+		email : 'tafiyaliza@april.biz' 
+	},
+	{
+		id:4,
+		name : 'Ganesh',
+		username : 'wizard',
+		email : 'ganeshwizard@april.biz' 
+	},
+	{
+		id:5,
+		name : 'Thanish',
+		username : 'tokiyo',
+		email : 'thanishtokiyo@april.biz' 
+	},
+	{
+		id:6,
+		name : 'Dev',
+		username : 'Barberian',
+		email : 'devBarberian@april.biz' 
+	},
+	{
+		id:7,
+		name : 'Barbie',
+		username : 'Liza',
+		email : 'salvanliza@april.biz' 
+	},
+	{
+		id:8,
+		name : 'vikas',
+		username : 'Liza',
+		email : 'likashliza@april.biz' 
+	},
+	{
+		id:9,
+		name : 'pragyan',
+		username : 'Liza',
+		email : 'pragyanliza@april.biz' 
+	},
+	];
